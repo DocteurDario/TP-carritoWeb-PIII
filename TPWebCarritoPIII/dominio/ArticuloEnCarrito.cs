fc47e8a7d982cebc34b9305fc7168e6bf45e902c
nahuelRomero1 <naahuel96@hotmail.com>
2023-10-13 15:09:13 -0300

    se agrego clase carrito y clase articuloencarrito

---
+++ TPWebCarritoPIII/dominio/ArticuloEnCarrito.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    class ArticuloEnCarrito
+    {
+        public ArticuloEnCarrito(int IdArt)
+        {
+            idArt = IdArt;
+            Cantidad = 1;
+        }
+
+        public int idArt { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
---
+++ TPWebCarritoPIII/dominio/Carrito.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    class Carrito
+    {
+        List<ArticuloEnCarrito> listaArticulo = new List<ArticuloEnCarrito>();
+
+        public void agregarArticulo(ArticuloEnCarrito art)
+        {
+            ArticuloEnCarrito existingArticle = listaArticulo.Find(a => a.idArt == art.idArt);
+            if (existingArticle != null)
+            {
+                existingArticle.Cantidad++;
+            }
+            else
+            {
+                listaArticulo.Add(art);
+            }
+        }
+
+        public void eliminarArticulo(ArticuloEnCarrito art)
+        {
+            ArticuloEnCarrito existingArticle = listaArticulo.Find(a => a.idArt == art.idArt);
+            if (existingArticle != null && existingArticle.Cantidad > 1)
+            {
+                existingArticle.Cantidad--;
+            }
+            else if (existingArticle != null && existingArticle.Cantidad == 1)
+            {
+                RemoverArticle(art);
+            }
+        }
+
+
+        public void RemoverArticle(ArticuloEnCarrito art)
+        {
+            ArticuloEnCarrito current = GetArticulo(art.idArt);
+            listaArticulo.Remove(current);
+        }
+
+        public ArticuloEnCarrito GetArticulo(int id)
+        {
+            return listaArticulo.Find(a => a.idArt == id);
+        }
+
+        public bool HasArticleId(int id)
+        {
+            return listaArticulo.Contains(GetArticulo(id));
+        }
+
+        public int ObtenerTotal()
+        {
+            int total = 0;
+            foreach (var article in listaArticulo)
+            {
+                total += article.Cantidad;
+            }
+            return total;
+        }
+
+        public int ObtenerCantidadArticulos(int id)
+        {
+            return GetArticulo(id).Cantidad;
+        }
+    }
+}
